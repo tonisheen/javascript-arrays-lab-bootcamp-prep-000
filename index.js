38b0b3b8933ba3adc82f07bbf280b0f7861d23ea
newtonmigosi@gmail.com <newtonmigosi@gmail.com>
2018-04-15 16:38:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,5 +29,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return [kittens]
+  return kittens.slice(0, kittens.length - 2)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
 }
